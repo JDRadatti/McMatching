beb16d0c81c348358ac3bdb7146744bd5a07265d
Justin Radatti <jdradatti@gmail.com>
2021-02-19 14:34:11 -0800

    ADD: help menu

---
+++ Assets/Scripts/CardScripts/DisplayHelpText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayHelpText : MonoBehaviour
+{
+    public GameObject help;
+    private bool displayed;
+
+    private void Start()
+    {
+        displayed = false;
+        help.SetActive(false);
+    }
+    public void DisplayText()
+    {
+        if (displayed == false)
+        {
+            help.SetActive(true);
+            displayed = true; 
+        }
+        else
+        {
+            help.SetActive(false);
+            displayed = false;
+        }
+        
+    }
+
+}
